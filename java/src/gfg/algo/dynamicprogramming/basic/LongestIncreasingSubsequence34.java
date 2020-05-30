aa69981d3ce94245539c7c71cbf2607b40b363ee
Rahul Dev Gupta <rahuldevg11@gmail.com>
2020-05-30 19:11:39 +0530

    lis

--- java/src/gfg/algo/dynamicprogramming/basic/LongestIncreasingSubsequence34.java
+++ java/src/gfg/algo/dynamicprogramming/basic/LongestIncreasingSubsequence34.java
@@ -1,6 +1,8 @@
 package gfg.algo.dynamicprogramming.basic;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class LongestIncreasingSubsequence34 {
 
@@ -8,18 +10,39 @@ public class LongestIncreasingSubsequence34 {
     {
         int lis[]=new int[arr.length];
 
-        Arrays.fill(arr,1);
+        Arrays.fill(lis,1);
 
-        
+        int i=1,j=0;
 
+        while(i<arr.length)
+        {
+            if (arr[i]>arr[j])
+            {
+                lis[i]=Math.max(lis[i],lis[j]+1);
+            }
+            j++;
+            if (i==j)
+            {
+                j=0;
+                i++;
+            }
+        }
 
+        for (int x:lis)
+            System.out.println(x);
 
+//        Integer brr[] = Arrays.stream(lis).boxed().toArray(Integer[]::new);
+//
+//        )
 
+//        Arrays.sort(brr,Collections.reverseOrder());
+        Arrays.sort(lis);
+        return lis[lis.length-1];
+    }
 
-
-
-
-
-        return 1;
+    public static void main(String args[])
+    {
+        int arr[]= {3,4,-1,0,6,2,3};
+        LIS(arr);
     }
 }
