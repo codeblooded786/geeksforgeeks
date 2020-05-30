476715a95cdb2302723bf01f814b5eeb8e0f9e85
Rahul Dev Gupta <rahuldevg11@gmail.com>
2020-05-30 15:06:52 +0530

    notes

--- java/src/gfg/algo/dynamicprogramming/basic/LongestIncreasingSubsequence34.java
+++ java/src/gfg/algo/dynamicprogramming/basic/LongestIncreasingSubsequence34.java
@@ -1,4 +1,25 @@
 package gfg.algo.dynamicprogramming.basic;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence34 {
+
+    public static int LIS(int arr[])
+    {
+        int lis[]=new int[arr.length];
+
+        Arrays.fill(arr,1);
+
+        
+
+
+
+
+
+
+
+
+
+        return 1;
+    }
 }
